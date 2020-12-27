463d14e4d1c8c5101e9e99d2d98b9e56eca73016
tjards <travis.jardine@gmail.com>
2020-12-27 17:15:23 -0500

    cleaning things up for next bound

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ import animation
 import ctrl_flock as flock
 import dynamics_node as node
 
-#%% Simulation Parameters
-# -----------------------
+#%% Setup Simulation
+# ------------------
 Ti = 0          # initial time
-Tf = 30         # final time 
+Tf = 10         # final time 
 Ts = 0.02       # sample time
 nVeh = 10       # number of vehicles
 nObs = 5        # number of obstacles
@@ -64,16 +64,14 @@ error = state[0:3,:] - targets[0:3,:]
 # Obstacles
 # --------
 obstacles = np.zeros((4,nObs))
-obstacles[0,:] = 5*iSpread*(np.random.rand(1,nObs)-0.5)    # position (x)
-obstacles[1,:] = 5*iSpread*(np.random.rand(1,nObs)-0.5)    # position (y)
-obstacles[2,:] = 5*iSpread*(np.random.rand(1,nObs)-0.5)    # position (z)
-obstacles[3,:] = np.random.rand(1,nObs)+0.5                  # radii of obstacle(s)
+obstacles[0,:] = iSpread*(np.random.rand(1,nObs)-0.5)    # position (x)
+obstacles[1,:] = iSpread*(np.random.rand(1,nObs)-0.5)    # position (y)
+obstacles[2,:] = iSpread*(np.random.rand(1,nObs)-0.5)    # position (z)
+obstacles[3,:] = np.random.rand(1,nObs)+0.5              # radii of obstacle(s)
 
 
-#%% Run simulation 
-# ----------------
-
-# initialize 
+#%% Run Simulation
+# ----------------------
 t = Ti
 i = 1
 nSteps = int(Tf/Ts+1)
@@ -82,54 +80,53 @@ states_all     = np.zeros([nSteps, len(state), nVeh])
 cmds_all       = np.zeros([nSteps, len(cmd), nVeh])
 targets_all    = np.zeros([nSteps, len(targets), nVeh])
 obstacles_all  = np.zeros([nSteps, len(obstacles), nObs])
-
-
-# store first steps
 t_all[0]                = Ti
 states_all[0,:,:]       = state
 cmds_all[0,:,:]         = cmd
 targets_all[0,:,:]      = targets
 obstacles_all[0,:,:]    = obstacles
 
-# run
 while round(t,3) < Tf:
   
-    # move the target
+    # Evolve the target
+    # -----------------
     targets[0,:] = targets[0,:] + 0.001
     targets[1,:] = targets[1,:] + 0.002
     targets[2,:] = targets[2,:] + 0.0005
 
 
-    # evolve the states (note: need to compute heading here at some point)
+    # Evolve the states
+    # -----------------
     state = node.evolve(Ts, state, cmd)
     
-    # store
+    # Store results
+    # -------------
     t_all[i]                = t
     states_all[i,:,:]       = state
     cmds_all[i,:,:]         = cmd
     targets_all[i,:,:]      = targets
     obstacles_all[i,:,:]    = obstacles
     
-    #increment 
+    # Increment 
+    # ---------
     t += Ts
     i += 1
+        
+    # Compute commands (next step)
+    # ----------------------------
+    states_q = state[0:3,:]     # positions
+    states_p = state[3:6,:]     # velocities 
+    d = 1                       # lattice scale (distance between a-agents)
+    r = 1.2*d                   # interaction range of a-agents
+    d_prime = 0.6*d             # distance between a- and b-agents
+    r_prime = 1.2*d_prime       # interaction range of a- and b-agents
     
-    # command for the next time step (target)
-    #cmd, error = flock.controller(Ts, i, state,cmd, nVeh, targets, error)
-    
-    # flocking part
-    states_q = state[0:3,:]
-    states_p = state[3:6,:]
-    d = 1
-    r = 1.2*d
-    d_prime = 0.6*d
-    r_prime = 1.2*d_prime 
-   
     cmd = flock.commands(states_q, states_p, obstacles, r, d, r_prime, d_prime, targets[0:3,:], targets[3:6,:])
     
     
     
-#%% plot
+#%% Produce animation of simulation
+# ---------------------------------
 
 ani = animation.animateMe(Ts, t_all, states_all, cmds_all, targets_all[:,0:3,:], obstacles_all)
 #plt.show()    
