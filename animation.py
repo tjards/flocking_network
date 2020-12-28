added floor plane, fixed normalization of a

animation.py
@@ -17,7 +17,7 @@ writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
 numFrames = 10 # frame rate (bigger = slower)
 tail = 8
 
-def animateMe(Ts, t_all, states_all, cmds_all, targets_all, obstacles_all, r, d):
+def animateMe(Ts, t_all, states_all, cmds_all, targets_all, obstacles_all, r, d, walls_plots):
     
     # pull out positions
     nVeh = states_all.shape[2]
@@ -79,6 +79,11 @@ def animateMe(Ts, t_all, states_all, cmds_all, targets_all, obstacles_all, r, d)
     
     #lattice = ax.plot([], [], [], '-', lw=1, color='cyan')
     lattices = []
+    
+    # draw plane
+    xx, yy = np.meshgrid(np.linspace(mid_x-maxRange, mid_x+maxRange, 10), np.linspace(mid_y-maxRange, mid_y+maxRange, 10))
+    zz = (-walls_plots[0,0] * xx - walls_plots[1,0] * yy + walls_plots[3,0] * 1.) / walls_plots[2,0]
+    ax.plot_wireframe(xx, yy, zz, color=[0.8, 0.8, 0.8])
     
     for i in range (0, nVeh):
         

ctrl_flock.py
@@ -119,6 +119,7 @@ def commands(states_q, states_p, obstacles, walls, r, d, r_prime, d_prime, targe
             mu = np.divide(obstacles[3, k_obstacle],normo)
             # compute bold_a_k (for the projection matrix)
             bold_a_k = np.divide(states_q[:,k_node]-obstacles[0:3,k_obstacle],normo)
+            bold_a_k = np.array(bold_a_k, ndmin = 2)
             # compute projection matrix
             P = np.identity(states_p.shape[0]) - np.dot(bold_a_k,bold_a_k.transpose())
             # compute beta-agent position and velocity
@@ -135,7 +136,7 @@ def commands(states_q, states_p, obstacles, walls, r, d, r_prime, d_prime, targe
         for k_wall in range(walls.shape[1]):
             
             # define the wall
-            bold_a_k = walls[0:3,k_wall]    # normal vector
+            bold_a_k = np.array(np.divide(walls[0:3,k_wall],np.linalg.norm(walls[0:3,k_wall])), ndmin=2).transpose()    # normal vector
             y_k = walls[3:6,k_wall]         # point on plane
             # compute the projection matrix
             P = np.identity(y_k.shape[0]) - np.dot(bold_a_k,bold_a_k.transpose())

main.py
@@ -27,10 +27,10 @@ import dynamics_node as node
 #%% Setup Simulation
 # ------------------
 Ti = 0          # initial time
-Tf = 30         # final time 
+Tf = 10         # final time 
 Ts = 0.02       # sample time
 nVeh = 20       # number of vehicles
-nObs = 10        # number of obstacles
+nObs = 1        # number of obstacles
 iSpread = 10     # initial spread of vehicles 
 
 # Vehicles states
@@ -73,11 +73,16 @@ obstacles[3,:] = np.random.rand(1,nObs)+0.5              # radii of obstacle(s)
 # -----------------------
 # need to compute the normal and point (cross product)
 nWalls = 1  
-walls = np.zeros((6,nWalls))                        # just do 1 for now
+walls = np.zeros((6,nWalls)) 
+walls_plots = np.zeros((4,nWalls)) 
+
+# horizontal wall
+wallh = 15
+                       
 # define 3 points on the plane (this one is horizontal)
-wallp1 = np.array([0, 0, 0])
-wallp2 = np.array([1, 1, 0])
-wallp3 = np.array([2, 3, 0])
+wallp1 = np.array([0, 0, wallh])
+wallp2 = np.array([5, 10, wallh+0.01])
+wallp3 = np.array([20, 30, wallh])
 # define two vectors on the plane
 v1 = wallp3 - wallp1
 v2 = wallp2 - wallp1
@@ -87,9 +92,10 @@ walla, wallb, wallc = wallcp
 # compute rest of equation for plane
 walld = np.dot(wallcp, wallp3)
 # store as walls
-walls[0:3] = np.array(wallcp, ndmin=2).transpose()
-walls[3:6] = np.array(wallp1, ndmin=2).transpose()
-
+walls[0:3,0] = np.array(wallcp, ndmin=2)#.transpose()
+walls[3:6,0] = np.array(wallp1, ndmin=2)#.transpose()
+#walls
+walls_plots[:,0] = np.array([walla, wallb, wallc, walld])
 
 #%% Run Simulation
 # ----------------------
@@ -149,7 +155,7 @@ while round(t,3) < Tf:
 #%% Produce animation of simulation
 # ---------------------------------
 
-ani = animation.animateMe(Ts, t_all, states_all, cmds_all, targets_all[:,0:3,:], obstacles_all, r, d)
+ani = animation.animateMe(Ts, t_all, states_all, cmds_all, targets_all[:,0:3,:], obstacles_all, r, d, walls_plots)
 #plt.show()    
 
 